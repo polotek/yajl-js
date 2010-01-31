8ec22261b3ae252e90414c84a6afb3e56c567ac3
Nikhil Marathe <nsm.nikhil@gmail.com>
2010-01-31 13:47:39 +0530

    Added Signatures for all Callbacks

--- src/callbacks.h
+++ src/callbacks.h
@@ -5,11 +5,17 @@
 #include <yajl/yajl_parse.h>
 
 namespace yajljs {
-
-int onNull( void *ctx );
-
-int onBoolean( void *ctx, int b );
-
+int OnNull( void *ctx );
+int OnBoolean( void *ctx, int b );
+int OnInteger( void *ctx, long b );
+int OnDouble( void *ctx, double b );
+int OnNumber( void *ctx, const char *numberVal, unsigned int numberLen );
+int OnString( void *ctx, const unsigned char *stringVal, unsigned int stringLen );
+int OnStartMap( void *ctx );
+int OnMapKey( void *ctx, const unsigned char *key, unsigned int stringLen );
+int OnEndMap( void *ctx );
+int OnStartArray( void *ctx );
+int OnEndArray( void *ctx );
 }
 
 #endif
